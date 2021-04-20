89c894407163ef5553d4567182417102b8b3dec8
shsmink <79309340+shsmink@users.noreply.github.com>
2021-04-20 13:50:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 console.log("Hello Winc Academy")
     let name = "Samantha";
     console.log(name);
-    // 6. Bij het gebruik van 'if' wordt er een andere code aangeroepen als er sprake is van een afwijkende waarde, volgens mij lukt dit alleen met getallen? 
+    // 6. Er kan geen variable worden gemaakt met de naam if, omdat if al zijn eigen functie heeft in Javascript
     let sum = 4 + 4;
     console.log("sum:", sum);
+    // sum with numbers is dus mogelijk, geeft gewoon als number (4+4=) 8.
     let sumWithStrings = "4" + "4";
     console.log("sum with strings:", sumWithStrings);
+    // sum with strings is niet mogelijk. In dit geval worden de twee elementen gewoon achter elkaar geplaatst, in dit geval "44"
     let subtraction = 8 - 3;
     console.log("subtraction:", subtraction);
     let multiplication = 9 * 5;
